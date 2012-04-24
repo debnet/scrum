33a61fc9480db809c2f3cc668cb72ddf81486d8d
Marc DEBUREAUX <marc@debnet.fr>
2012-04-24 19:35:34 +0200

    Updating deprecated middlewares and processors

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ TEMPLATE_DEBUG = True
 
 # Administrators and managers
 ADMINS = (
-    ('Marc DEBUREAUX', 'marc@debnet.fr'),
+    ('Marc DEBUREAUX', 'scrum@debnet.fr'),
 )
 MANAGERS = ADMINS
 
@@ -120,14 +120,13 @@ SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#   'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
+#   'django.middleware.csrf.CsrfResponseMiddleware',
     'django.middleware.common.CommonMiddleware',
 #   'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -136,7 +135,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
